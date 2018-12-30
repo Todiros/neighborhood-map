d0c598bff6df71bd195cf22926374052023cabb3
Tony Borisov <tony.d.borisov@gmail.com>
2018-12-30 09:05:39 +0200

    feat: adding station list filtering funtionality in the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
 
 class SideBar extends Component {
+    state = {
+        filteredStations: []
+    }
+
+    filterStations = (filter) => {
+        let stationsList = []
+        
+        stationsList = this.props.stations.filter(station => {
+            if (filter) {
+                if (station.title.toLowerCase().includes(filter.toLowerCase())) {
+                    return station
+                }
+            } else {
+                return  station
+            }
+        })
+
+        return stationsList
+    }
+
+    onFilterClick = () => {
+        const fieldInput = document.getElementById('station-filter-input').value
+        const filteredStations = this.filterStations(fieldInput)
+
+        this.setState({ filteredStations })
+    }
+
     render () {
         const deviceType = this.props.deviceType || 'on-mobile'
+        const stations = this.state.filteredStations.length >= 1 ? this.state.filteredStations : this.props.stations
 
         return (
             <section id="side-bar" className={deviceType}>
@@ -12,15 +40,17 @@ class SideBar extends Component {
                 <nav id="stations-list-container">
                     <h2 className="nav-title">Stations List</h2>
                     <ul className="stations-list">
-                        {this.props.stations.map(station => 
-                            <li key={station.lat + station.lng}>
-                                <a className='station-link' href='javascript:void(0)'>{station.title}</a>
-                            </li>
-                        )}
+                        {
+                            stations.map(station =>
+                                <li key={station.lat + station.lng}>
+                                    <a className='station-link' href='javascript:void(0)'>{station.title}</a>
+                                </li>
+                            )
+                        }
                     </ul>
                     <div className="filter-wrap">
-                        <input type="text" placeholder="NAME / ADDRESS"></input>
-                        <button className="button filter-button">FILTER</button>
+                        <input id='station-filter-input' type="text" placeholder="NAME / ADDRESS"></input>
+                        <button className="button filter-button" onClick={this.onFilterClick}>FILTER</button>
                     </div>
                 </nav>
             </section>

5c287938ce3c0bbb7a2cbcf7c8c1a7513692cba6
Tony Borisov <tony.d.borisov@gmail.com>
2019-01-07 09:15:29 +0200

    feat: adds more properties to the station object, changes the name of the Get function accordingly

--- src/js/StationsAPI.js
+++ src/js/StationsAPI.js
@@ -2,28 +2,56 @@ import objArr from '../res/objArr'
 import { openChargeKey } from './API_Keys'
 
 class Station {
-    constructor({AddressInfo}) {
-        let {
+    constructor( { UsageType, AddressInfo, StatusType, Connections }) {
+        const {
             Title: title, 
-            AddressLine1: address, 
+            AddressLine1: address,
             Longitude: lng,
-            Latitude: lat
+            Latitude: lat,
+            Town: town,
+            Postcode: postcode
         } = AddressInfo
+
+        const {
+            IsOperational: isOperational,
+            Title: statusTitle
+        } = StatusType
+
+        const connectors = Connections.map((x) => {
+            return x.ConnectionType ? x.ConnectionType.Title || 'NA' : 'NA'
+        })
+
+        this.id = lng + lat
         this.title = title || 'NA'
         this.address = address || 'NA'
         this.lng = lng || 0
         this.lat = lat || 0
+        this.town = town || 'NA'
+        this.postcode = postcode || 'NA'
+        this.usageType = UsageType ? 
+                            UsageType.Title || 'NA' : 'NA' 
+        this.isOperational = isOperational || 'NA'
+        this.statusTitle = statusTitle || 'NA'
+        this.connectors = connectors
     }
 
-    GetAddressInfo() {
+    GetInfo() {
         return {
+            id: this.id,
             title: this.title,
             address: this.address,
             lat: this.lat,
-            lng: this.lng
+            lng: this.lng,
+            town: this.town, 
+            postcode: this.postcode,
+            usageType: this.usageType,
+            isOperational: this.isOperational,
+            statusTitle: this.statusTitle,
+            connectors: this.connectors
         }
     }
 
+    // OUTDATED
     LogObject() {
         console.log(
             this.title,
@@ -37,7 +65,7 @@ class Station {
 const getTest = () => {
     let stations = []
     Array.prototype.forEach.call(objArr, station => {
-        station = new Station(station).GetAddressInfo()
+        station = new Station(station).GetInfo()
 
         stations.push(station)
     })
@@ -50,7 +78,7 @@ const getAll = () =>
         .then(res => res.json())
         .then(data => {
             const stations = data.map(station => {
-                return new Station(station).GetAddressInfo()
+                return new Station(station).GetInfo()
             })
 
             return stations
